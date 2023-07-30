462ea98b15deba011319cffbed3cc9035c5b9f08
Itamar Ben-Atar <ibenatar96@gmail.com>
2023-07-30 20:54:14 +0300

    model inference

--- model_inference.py
+++ model_inference.py
@@ -3,12 +3,13 @@ import numpyro
 import numpyro.distributions as dist
 import jax
 import numpy
-import scipy
+import itertools
 import time
 
 import utils
 
 # numpyro.set_platform("gpu")
+numpyro.set_host_device_count(4)
 
 model_beta_parameters = {}
 func_numeric_mapping = {}
@@ -22,31 +23,22 @@ func_numeric_mapping = {}
 # What we are expecting to see is an increase in the acc. rewards.
 # Order is -> 1. Run Model, 2. Gather logs / acc. reward, 3. Update Posterior (model_parameters) and repeat 1.,2.,3...
 
-def min_state(state):
-    min_s = [0,0,0,0,0,0,0,0,0]
-    for i in range(3):
-        for j in range(3):
-            if state[i][j] == 'X':
-                min_s[i*3 + j] = -1
-            elif state[i][j] == 'O':
-                min_s[i*3 + j] = 1
-    return min_s
 
 def parse_obs(obs_file):
     """
     Parses the Observation File, returns a Dictionary where: Key: (Function Number, Action Parameter), Value: List of
     observations (list composed of 1's and 0's - 1 indicate that State != Next State, and 0 else).
     Notice that it ignores actions that are in utils.IGNORE_ACTIONS. In our case - 'opponent_mark' is ignored.
-    For example: obs_map = [
-    ((0, 5), [1, 1, 0, 0, 0, 0, 0, 1]),
-    ((0, 1), [1, 1, 1, 1]),
-    ((0, 8), [1, 1, 1, 1, 1, 1]),
-    ((0, 4), [1, 1, 1, 1, 1]),
-    ((0, 7), [1, 1, 1, 1, 1, 1, 1]),
-    ((0, 3), [1, 1, 1, 1, 1]),
-    ((0, 6), [1, 1, 1, 1, 1, 1, 1]),
-    ((0, 2), [1, 1, 1, 1, 1, 1, 1, 1]),
-    ((0, 9), [1, 1, 1])]
+    For example: obs_map = {
+    (0, 5): [1, 1, 0, 0, 0, 0, 0, 1],
+    (0, 1): [1, 1, 1, 1],
+    (0, 8): [1, 1, 1, 1, 1, 1],
+    (0, 4): [1, 1, 1, 1, 1],
+    (0, 7): [1, 1, 1, 1, 1, 1, 1],
+    (0, 3): [1, 1, 1, 1, 1],
+    (0, 6): [1, 1, 1, 1, 1, 1, 1],
+    (0, 2): [1, 1, 1, 1, 1, 1, 1, 1],
+    (0, 9): [1, 1, 1]}
 
     Where 0 is mapped to 'ai_mark' function in global func_numeric_mapping variable. This mapping exists because NumPyro
     MCMC expects numeric values when running inference.
@@ -81,50 +73,47 @@ def parse_obs(obs_file):
             if not (func_numeric, action_param) in model_beta_parameters:
                 model_beta_parameters[(func_numeric, action_param)] = {'alpha': 1, 'beta': 1}
 
-    result = obs_map.items()
-    obs_map = list(result)
-    return obs_map
+    sorted_dict = dict(sorted(obs_map.items(), key=lambda item: item[0]))
+    return sorted_dict
 
 
 def fill_post_model_params(posterior, prior):
-    for action in prior.keys():
-        if action not in posterior:
-            posterior[action] = prior[action]
+    posterior = {action: prior[action] for action in prior.keys() if action not in posterior}
 
 
 def ai_model(obs=None, nobs=None):
     """
     (Tic-Tac-Toe) AI Agent model with NumPyro.
     p is a dictionary mapping (function, action parameter) -> sample from Beta distribution using (alpha, beta).
-    obs = [
-    ((0, 5), [1, 1, 0, 0, 0, 0, 0, 1]),
-    ((0, 1), [1, 1, 1, 1]),
-    ((0, 8), [1, 1, 1, 1, 1, 1]),
-    ((0, 4), [1, 1, 1, 1, 1]),
-    ((0, 7), [1, 1, 1, 1, 1, 1, 1]),
-    ((0, 3), [1, 1, 1, 1, 1]),
-    ((0, 6), [1, 1, 1, 1, 1, 1, 1]),
-    ((0, 2), [1, 1, 1, 1, 1, 1, 1, 1]),
-    ((0, 9), [1, 1, 1])]
+    obs = {
+    (0, 5): [1, 1, 0, 0, 0, 0, 0, 1],
+    (0, 1): [1, 1, 1, 1],
+    (0, 8): [1, 1, 1, 1, 1, 1],
+    (0, 4): [1, 1, 1, 1, 1],
+    (0, 7): [1, 1, 1, 1, 1, 1, 1],
+    (0, 3): [1, 1, 1, 1, 1],
+    (0, 6): [1, 1, 1, 1, 1, 1, 1],
+    (0, 2): [1, 1, 1, 1, 1, 1, 1, 1],
+    (0, 9): [1, 1, 1]}
     """
     # p ~ Beta(alpha, beta)
     global model_beta_parameters
     p = {}
-    for (func, action_param) in model_beta_parameters.keys():
-        alpha = model_beta_parameters[(func, action_param)]['alpha']
-        beta = model_beta_parameters[(func, action_param)]['beta']
-        p[(func, action_param)] = numpyro.sample(f"p({str(func)}, {str(action_param)})", dist.Beta(alpha, beta))
-    for (func, action_param), p_i in p.items():
+    for key in model_beta_parameters.keys():
+        alpha = model_beta_parameters[key]['alpha']
+        beta = model_beta_parameters[key]['beta']
+        p[key] = numpyro.sample(f"p{str(key)}", dist.Beta(alpha, beta))
+    for key, p_i in p.items():
         if obs is not None:
-            action_obs = [sublist[1] for sublist in obs if sublist[0] == (func, action_param)][0]
-            nobs = len(action_obs)
+            action_obs = obs[key]
+            action_obs = jax.numpy.array(action_obs)
+            nobs_i = len(action_obs)
         else:
             action_obs = None
             nobs = nobs
-        nobs_i = nobs[(func, action_param)]
-        with numpyro.plate("obs", size=nobs_i):
-            numpyro.sample(f"o({str(func)}, {str(action_param)})", dist.Bernoulli(p_i),
-                           obs=action_obs)
+            nobs_i = nobs[key]
+        with numpyro.plate(f"obs{str(key)}", size=nobs_i):
+            numpyro.sample(f"o{str(key)}", dist.Bernoulli(p_i), obs=action_obs)
 
 
 def prior_predictive(obs):
@@ -134,18 +123,21 @@ def prior_predictive(obs):
     """
     prior_predi = numpyro.infer.Predictive(ai_model, num_samples=10000)
     prior_samples = prior_predi(jax.random.PRNGKey(int(time.time() * 1E6)),
-                                nobs={key: len(value) for (key, value) in obs})
+                                nobs={key: len(value) for (key, value) in obs.items()})
     if utils.PLOT:
-        fig, axs = plt.subplots(3, 3, figsize=(12, 12))
+        num_rows, num_cols = largest_divisors(len(obs.keys()))
+        assert num_rows * num_cols == len(obs.keys())
+        fig, axs = plt.subplots(num_rows, num_cols, figsize=(12, 12))
         plt.subplots_adjust(hspace=0.5)
         plt.title("prior predictive")
-        for i in range(3):
-            for j in range(3):
-                ((func, action_param), action_obs) = obs[i % 3 + j % 3]
-                key = f"o({str(func)}, {str(action_param)})"
-                axs[i, j].set_title(f"Cell: {i * 3 + j + 1}")
+        obs_list = list(obs.items())
+        for i in range(num_rows):
+            for j in range(num_cols):
+                key, action_obs = obs_list[i*num_cols + j]
+                o_key = f"o{str(key)}"
+                axs[i, j].set_title(o_key)
                 axs[i, j].set_xlim(-1, len(action_obs) + 1)
-                axs[i, j].hist([sum(o) for o in prior_samples[key]], density=True, bins=len(action_obs) * 2 + 1,
+                axs[i, j].hist([sum(o) for o in prior_samples[o_key]], density=True, bins=len(action_obs) * 2 + 1,
                                label="imaginations")
                 axs[i, j].axvline(sum(action_obs), color="red", lw=2, label="observation")
                 axs[i, j].legend()
@@ -157,25 +149,46 @@ def inference(obs):
     """
     Runs Inference using MCMC.
     """
-    obs = jax.numpy.array(obs)
-
     nuts_kernel = numpyro.infer.NUTS(ai_model)
     mcmc = numpyro.infer.MCMC(
         nuts_kernel,
         num_warmup=500,
         num_chains=4,
         num_samples=5000)
-    mcmc.run(jax.random.PRNGKey(int(time.time() * 1E6)), obs=jax.numpy.array(obs))
+    mcmc.run(jax.random.PRNGKey(int(time.time() * 1E6)), obs=obs)
     mcmc.print_summary()
     return mcmc
 
 
-def posterior(mcmc):
+def largest_divisors(x):
+    # Initialize variables to store the two largest divisors
+    largest_divisor1 = 1
+    largest_divisor2 = x
+
+    # Find the two largest divisors of x
+    for i in range(2, int(x ** 0.5) + 1):
+        if x % i == 0:
+            largest_divisor1 = i
+            largest_divisor2 = x // i
+
+    return largest_divisor2, largest_divisor1
+
+
+def posterior(mcmc, obs):
     if utils.PLOT:
-        plt.figure(figsize=(10, 3))
+        num_rows, num_cols = largest_divisors(len(obs.keys()))
+        assert num_rows * num_cols == len(obs.keys())
+        fig, axs = plt.subplots(num_rows, num_cols, figsize=(12, 12))
+        plt.subplots_adjust(hspace=0.5)
         plt.title("posterior")
-        plt.xlabel("p")
-        plt.hist(mcmc.get_samples()['p'], density=True, bins="auto", label="approximate")
+        obs_list = list(obs.items())
+        for i in range(num_rows):
+            for j in range(num_cols):
+                key, action_obs = obs_list[i*num_cols + j]
+                p_key = f"p{str(key)}"
+                axs[i, j].set_title(p_key)
+                axs[i, j].set_xlabel("p")
+                axs[i, j].hist(mcmc.get_samples()[p_key], density=True, bins='auto')
         plt.show()
 
 
@@ -186,18 +199,21 @@ def posterior_predictive(obs, mcmc):
     """
     posterior_predi = numpyro.infer.Predictive(ai_model, posterior_samples=mcmc.get_samples())
     posterior_samples = posterior_predi(jax.random.PRNGKey(int(time.time() * 1E6)),
-                                        nobs={key: len(value) for (key, value) in obs})
+                                        nobs={key: len(value) for (key, value) in obs.items()})
     if utils.PLOT:
-        fig, axs = plt.subplots(3, 3, figsize=(12, 12))
+        num_rows, num_cols = largest_divisors(len(obs.keys()))
+        assert num_rows * num_cols == len(obs.keys())
+        fig, axs = plt.subplots(num_rows, num_cols, figsize=(12, 12))
         plt.subplots_adjust(hspace=0.5)
         plt.title("posterior predictive")
-        for i in range(3):
-            for j in range(3):
-                ((func, action_param), action_obs) = obs[i % 3 + j % 3]
-                key = f"o({str(func)}, {str(action_param)})"
-                axs[i, j].set_title(f"Cell: {i * 3 + j + 1}")
+        obs_list = list(obs.items())
+        for i in range(num_rows):
+            for j in range(num_cols):
+                key, action_obs = obs_list[i*num_cols + j]
+                o_key = f"o{str(key)}"
+                axs[i, j].set_title(o_key)
                 axs[i, j].set_xlim(-1, len(action_obs) + 1)
-                axs[i, j].hist([sum(o) for o in posterior_samples[key]], density=True, bins=len(action_obs) * 2 + 1,
+                axs[i, j].hist([sum(o) for o in posterior_samples[o_key]], density=True, bins=len(action_obs) * 2 + 1,
                                label="imaginations")
                 axs[i, j].axvline(sum(action_obs), color="red", lw=2, label="observation")
                 axs[i, j].legend()
@@ -210,33 +226,42 @@ def posterior_predictive(obs, mcmc):
 #     print(f"p_value = {p_value:.3f}")
 
 
-def summarize_posterior(mcmc):
+def summarize_posterior(mcmc, obs):
     """
     Summarizes Posterior, displays attributes such as mean, standard deviation, quantiles.
     """
-    p = mcmc.get_samples()["p"]
-    p_mean = p.mean()
-    p_stddev = p.std()
-    quantiles = [0, 0.025, 0.25, 0.5, 0.75, 0.975, 1]
-    pq = numpy.quantile(p, quantiles)
-    print(f"stat\tp\n-------------")
-    print(f"mean\t{p_mean:.3f}")
-    print(f"stddev\t{p_stddev:.3f}")
-    for i in range(len(quantiles)):
-        print(f"{quantiles[i] * 100:3.0f}%\t{pq[i]:.3f}")
+    distribution_stats = {}
     if utils.PLOT:
-        plt.figure(figsize=(10, 3))
-        plt.xlabel("p")
-        height, _, _ = plt.hist(p, histtype="step", lw=2, bins="auto", label="posterior")
-        plt.title(f"mean={p_mean:.3f}, stddev={p_stddev:.3f}")
-        plt.axvline(p_mean, ls="dashed", color="red", label="mean")
-        plt.fill_betweenx([0, height.max()], pq[1], pq[-2],
-                          color="red", alpha=0.1, label=f"{(quantiles[-2] - quantiles[1]) * 100:.0f}%")
-        plt.fill_betweenx([0, height.max()], pq[2], pq[-3],
-                          color="red", alpha=0.2, label=f"{(quantiles[-3] - quantiles[2]) * 100:.0f}%")
-        plt.legend()
+        num_rows, num_cols = largest_divisors(len(obs.keys()))
+        assert num_rows * num_cols == len(obs.keys())
+        fig, axs = plt.subplots(num_rows, num_cols, figsize=(12, 12))
+        plt.subplots_adjust(hspace=0.5)
+        obs_list = list(obs.items())
+        for i in range(num_rows):
+            for j in range(num_cols):
+                key, action_obs = obs_list[i*num_cols + j]
+                p_key = f"p{str(key)}"
+                p = mcmc.get_samples()[p_key]
+                p_mean = p.mean()
+                p_stddev = p.std()
+                distribution_stats[key] = {'mean': p_mean, 'stddev': p_stddev}
+                quantiles = [0, 0.025, 0.25, 0.5, 0.75, 0.975, 1]
+                pq = numpy.quantile(p, quantiles)
+                print(f"stat\t{p_key}\n-------------")
+                print(f"mean\t{p_mean:.3f}")
+                print(f"stddev\t{p_stddev:.3f}")
+                for q in range(len(quantiles)):
+                    print(f"{quantiles[q] * 100:3.0f}%\t{pq[q]:.3f}")
+                print("\n")
+                height, _, _ = axs[i, j].hist(p, histtype="step", lw=2, bins="auto", label="posterior")
+                axs[i, j].set_title(f"mean={p_mean:.3f}, stddev={p_stddev:.3f}")
+                axs[i, j].axvline(p_mean, ls="dashed", color="red", label="mean")
+                axs[i, j].fill_betweenx([0, height.max()], pq[1], pq[-2],
+                                        color="red", alpha=0.1, label=f"{(quantiles[-2] - quantiles[1]) * 100:.0f}%")
+                axs[i, j].fill_betweenx([0, height.max()], pq[2], pq[-3],
+                                        color="red", alpha=0.2, label=f"{(quantiles[-3] - quantiles[2]) * 100:.0f}%")
         plt.show()
-    return p_mean, p_stddev
+    return distribution_stats
 
 
 def ML(obs_file, prior_model_parameters=None):
@@ -248,17 +273,21 @@ def ML(obs_file, prior_model_parameters=None):
     global model_beta_parameters
     prior_predi = prior_predictive(obs_map)
     mcmc = inference(obs_map)
-    posterior(mcmc)
+    posterior(mcmc, obs_map)
     posterior_predi = posterior_predictive(obs_map, mcmc)
     # p_value(obs, posterior_samples)
-    p_mean, p_stddev = summarize_posterior(mcmc)
-    # sample = numpy.random.choice(mcmc.get_samples()["p"])
-    # print(f"sample: {sample}\n")
-    # posterior_model_parameters[(func, action_parameter)] = sample
-    # model_beta_parameters[(func, action_parameter)]['alpha'] = p_mean * (
-    #         ((p_mean * (1 - p_mean)) / p_stddev) - 1)
-    # model_beta_parameters[(func, action_parameter)]['beta'] = (1 - p_mean) * (
-    #         ((p_mean * (1 - p_mean)) / p_stddev) - 1)
+    dist_stats = summarize_posterior(mcmc, obs_map)
+    for key, _ in obs_map.items():
+        p_key = f"p{str(key)}"
+        sample = numpy.random.choice(mcmc.get_samples()[p_key])
+        print(f"sample: {sample}")
+        p_mean = dist_stats[key]['mean']
+        p_stddev = dist_stats[key]['stddev']
+        posterior_model_parameters[key] = sample
+        model_beta_parameters[key]['alpha'] = p_mean * (
+                ((p_mean * (1 - p_mean)) / p_stddev) - 1)
+        model_beta_parameters[key]['beta'] = (1 - p_mean) * (
+                ((p_mean * (1 - p_mean)) / p_stddev) - 1)
     fill_post_model_params(posterior_model_parameters, prior_model_parameters)
     print(f"posterior model parameters: {posterior_model_parameters}")
     return posterior_model_parameters
